be7b0c024e2bf744f8d65ca353b256c38ea1d49f
Dan Schepers <daniel.schepers@rallyhealth.com>
2022-04-05 20:55:50 -0500

    attempt emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  host = 'mcschepers-wedding.com'
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            Rails.application.secrets.gmail['user_name'],
+    password:             Rails.application.secrets.gmail['password'],
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 end
